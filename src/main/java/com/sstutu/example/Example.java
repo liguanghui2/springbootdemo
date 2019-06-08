e148d20df7cf314299c566d51ccb977e084eb526
liguanghui <leetutuinfo@gmail.com>
2019-06-08 21:30:36 +0800

    commit

--- src/main/java/com/sstutu/example/Example.java
+++ src/main/java/com/sstutu/example/Example.java
@@ -3,11 +3,18 @@ package com.sstutu.example;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 public class Example {
 
     @RequestMapping(value = "/hello")
     public String hello() {
+        List<String> list=new ArrayList<String>();
+        Map map=new HashMap();
         return "hello word";
     }
 }
